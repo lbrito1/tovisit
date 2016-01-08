43725acc7af5f1ea60176233051adb01c348791b
Leonardo <lbrito@gmail.com>
2016-01-08 00:34:25 -0300

    Close window if no city was found

--- app/controllers/geocoder_controller.rb
+++ app/controllers/geocoder_controller.rb
@@ -2,8 +2,14 @@ class GeocoderController < ApplicationController
 
 	def decode
 		addr = Geocoder.search([params['latitude'],params['longitude']]).first
-		respond_to do |format|
-		  format.json { render json: { city: addr.city } }
+		if addr.city.nil?			
+			respond_to do |format|
+			  format.json { render json: 'No city found', status: 400 }
+			end
+		else
+			respond_to do |format|
+			  format.json { render json: { city: addr.city } }
+			end
 		end
 	end
 
