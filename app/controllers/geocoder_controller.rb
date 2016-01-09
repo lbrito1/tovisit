d0dfc1aade7c6f588b3674e728c047b95c0fde3a
Leonardo <lbrito@gmail.com>
2016-01-09 15:31:54 -0300

    Fixing nilref

--- app/controllers/geocoder_controller.rb
+++ app/controllers/geocoder_controller.rb
@@ -2,7 +2,7 @@ class GeocoderController < ApplicationController
 
 	def decode
 		addr = Geocoder.search([params['latitude'],params['longitude']]).first
-		if addr.city.nil?			
+		if addr.nil? || addr.city.nil?			
 			respond_to do |format|
 			  format.json { render json: 'No city found', status: 400 }
 			end
